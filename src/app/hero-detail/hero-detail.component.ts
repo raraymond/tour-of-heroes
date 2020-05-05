b49dab733aab33a460b9898ccbef84c91e3c44dd
Ryan Raymond <ryan@Ryans-iMac.local>
2020-05-05 16:11:02 -0400

    You added the necessary dependencies to use HTTP in the app. You refactored HeroService to load heroes from a web API. You extended HeroService to support post(), put(), and delete() methods. You updated the components to allow adding, editing, and deleting of heroes. You configured an in-memory web API. You learned how to use observables.

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,25 +1,23 @@
 import { Component, OnInit, Input } from '@angular/core';
-import {Hero} from '../hero'
+import { Hero } from '../hero';
 
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
-import { HeroService }  from '../hero.service';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+  styleUrls: ['./hero-detail.component.css'],
 })
-
 export class HeroDetailComponent implements OnInit {
-
   @Input() hero: Hero;
   constructor(
     private route: ActivatedRoute,
     private heroService: HeroService,
     private location: Location
-    ) { }
+  ) {}
 
   ngOnInit(): void {
     this.getHero();
@@ -27,15 +25,16 @@ export class HeroDetailComponent implements OnInit {
 
   getHero(): void {
     const id = +this.route.snapshot.paramMap.get('id');
-    this.heroService.getHero(id)
-      .subscribe(hero => this.hero = hero)
+    this.heroService.getHero(id).subscribe((hero) => (this.hero = hero));
   }
-  goBack(): void{
-    this.location.back()
+  goBack(): void {
+    this.location.back();
   }
 
+  save(): void {
+    this.heroService.updateHero(this.hero).subscribe(() => this.goBack());
+  }
 }
-
 
 // The route.snapshot is a static image of the route information shortly after the component was created.
 
